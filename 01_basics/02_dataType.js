e9450e874e2d0171990ebeaef6f3282104577634
Akash keshri <keshriakash522@gmail.com>
2023-10-17 20:27:48 +0530

    dataTypes

---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,26 @@
+"use strict" // treat all JS code as newer version 
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(3
+    +3
+    );  // code readability should be high
+
+console.log("Akash")
+
+
+let name = "Akash"
+let age = 18
+let isLoggedIn = false
+
+// number => pow(2,53)
+// bigint
+// string => ""
+// boolean => true/false
+// null => standlone value
+// undefined 
+// symbol => unique
+// object
+
+console.log(typeof null);  // object
+console.lof(typeof undefined); // undefined
