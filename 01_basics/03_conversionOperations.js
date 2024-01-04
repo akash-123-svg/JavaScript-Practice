3530f491810c4d3500ad2a0452f3d0d49d29d871
Akash keshri <keshriakash522@gmail.com>
2024-01-04 13:26:20 +0530

    conversionOperations

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,24 @@
+let score="33"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+
+// "33" => number
+// "33abc" => NaN
+// true => 1 , false => 0
+
+let isLoggedIn=1
+
+let valueisloggedIn = Boolean(isLoggedIn)
+console.log(valueisloggedIn);
+
+
+// 1=>true, 0=>false;
+// "" => false;
+// "Akash" => true;
